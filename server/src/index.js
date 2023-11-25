fc508a981e06d8b365ded122e3a98dbe019b3e53
Rajat Sharma <rajatxd007@gmail.com>
2023-11-25 22:00:04 +0530

    Added code to Load Planets Data on Startup of Application

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,9 @@ const {ServerConfig} = require('./config');
 const app = express();
 const cors = require('cors');
 app.use(express.json());
+
+const { loadPlanetsData } = require('./models/planets')
+
 app.use(cors({
     origin : 'http://localhost:3000',
 }))
@@ -11,7 +14,12 @@ const apiroutes = require('./routes')
 app.use('/api',apiroutes);
 
 
-app.listen(ServerConfig.PORT,(req,res)=>{
-    console.log(`Backend App is running on ${ServerConfig.PORT}`);
-    console.log('request object is ',req);
-});
+async function startServer(){
+    await loadPlanetsData();
+    app.listen(ServerConfig.PORT,(req,res)=>{
+        console.log(`Backend App is running on ${ServerConfig.PORT}`);
+        console.log('request object is ',req);
+    });
+}
+
+startServer();
--- server/src/models/planets.js
+++ server/src/models/planets.js
@@ -1,32 +1,46 @@
-const { parse } = require('csv-parse');
+const path = require('path');
 const fs = require('fs');
-
+const { parse } = require('csv-parse');
 
 const habitablePlanets = [];
 
 function ishabitable(planet){
-    return planet['koi_prad'] < 1.6 && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    return planet['koi_prad'] < 1.6 && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_disposition'] === 'CONFIRMED';
 }
 
-fs.createReadStream('../data/kepler_data.csv')
+//binding the below code in a promise based syntax so that planets data will be available on startup of application
+
+function loadPlanetsData(){
+    return new Promise ((resolve,reject)=>{
+    fs.createReadStream(path.join(__dirname,'..','data','keplerData.csv'))
     .pipe(parse({
         comment: '#',
         columns : true,
     }))
     .on('data', (data)=>{
         if(ishabitable(data)){
+            //console.log(`${data} in search is this data`)
             habitablePlanets.push(data);
         }
     })
     .on('error' , (error)=>{
         console.log('Error Recieved in Reading Stream',error);
+        reject();
     })
     .on('end',()=>{
-        console.log(habitablePlanets.map((planet)=>{
-            return planet['kepler_name'];
-        }))
+        console.log(`${habitablePlanets.length} found in our search`);
+        console.log(habitablePlanets.map((data)=>{
+            return data['kepler_name'];
+        }));
+        resolve();
+
     })
+    })
+}
+
+
 
 module.exports = {
-    habitablePlanets,
+    planets : habitablePlanets,
+    loadPlanetsData,
 }
